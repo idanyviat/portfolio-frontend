06523d71bbf367c031e7a36461cd8019fdba0f8e
Dany <54440354+idanyviat@users.noreply.github.com>
2023-06-13 16:36:10 +0300

    Добавлены болванки для шаблона

---
+++ src/Layouts/Template.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <Header />
+        <main class="main">
+            <slot></slot>
+        </main>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from '../components/TemplateComponents/Header/Header.vue';
+import Footer from '../components/TemplateComponents/Footer/Footer.vue';
+export default {
+    name: 'Template',
+    components: {
+        Header,
+        Footer
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {}
+</style>
